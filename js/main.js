27f7c8c3412a9221aeec532d39ee3d0fce799a54
AlejandraEscobar3G <ale.fesico@gmail.com>
2016-10-26 08:08:53 -0500

    Subrayado, agregado y eliminado de listas

--- js/main.js
+++ js/main.js
@@ -1,46 +1,44 @@
 var boton = document.getElementById("agregar");
-boton.onclick = function () {agregarTarea()};
-function agregarTarea(){
-	var valorTarea = document.getElementById("tarea").value;
-	if(valorTarea == 0){
-		alert("Actividad inválida");
-		return false;
-	} else{
-		crearTarea(valorTarea);
-	}
-}
+boton.addEventListener("click", agregarTarea);
 
-function crearTarea(tarea){
-	tarea = document.createTextNode(tarea);
-	var pre = document.getElementById('Pre');
-	var parrafo = document.createElement("p");
-	var checkbox = document.createElement("input");
-	checkbox.setAttribute("type", "checkbox");
-	var elimina = document.createElement("i");
-	elimina.classList.add('glyphicon', 'glyphicon-trash');
-	parrafo.appendChild(checkbox);
-	parrafo.appendChild(tarea);
-	parrafo.appendChild(elimina);
-	pre.appendChild(parrafo);
-	if(checkbox.onclick){
-		tacharTarea()
+//Codigo que valida y agrega tareas
+function agregarTarea(){
+
+	var valorTarea = document.getElementById("tarea").value;
+	
+	if(valorTarea == 0){
+		alert("Escribe la tarea");
+		return false;
+	} else {
+		
+		var listaUl = document.getElementById("myUl"); //Trayendo UL
+		
+		var lista = document.createElement("LI"); // Creando LI
+		var check = document.createElement("input"); //Creando input
+		check.setAttribute("type", "checkbox"); // Definiendo checkbox
+		
+		var textoArea = document.createTextNode(valorTarea); //Haciendo nodo el texto 
+		var pe = document.createElement("p");
+		var spanTrash = document.createElement("span"); //Creando Span
+		spanTrash.classList.add("glyphicon", "glyphicon-trash"); //Agregando icono
+		// Usando appendChild();
+		pe.appendChild(textoArea);
+		lista.appendChild(check);
+		lista.appendChild(pe);
+		lista.appendChild(spanTrash);
+
+		listaUl.appendChild(lista);
+
+		spanTrash.onclick = function() {
+			lista.parentNode.removeChild(lista);
+		};
+		
+		check.onclick = function(){
+			pe.classList.toggle("subrayar");
+		};
+	
 	}
+	
+	document.getElementById("tarea").value = ""; //Quitando el valor de textarea
+
 }
-// function crearTarea(actividad){
-// 	var pre = document.getElementById('Pre'); //Obteniendo PRE
-// 	var lista = document.createElement("p");
-// 	var checkbox = document.createElement("input");
-// 	actividad = document.createTextNode(actividad);
-// 	checkbox.setAttribute("type", "checkbox"); //Creando el checkbox
-// 	//checkbox.addEventListener("click", tacharTarea(actividad)); //Agregando evento a checkbox
-// 	var elimina = document.createElement("i");
-// 	elimina.classList.add('glyphicon', 'glyphicon-trash'); //Creando icono de eliminar
-// 	lista.appendChild(checkbox);
-// 	lista.appendChild(actividad);
-// 	lista.appendChild(elimina);
-// 	pre.appendChild(lista);
-// }
-// function tacharTarea(subrayado){
-// 	var tachar = document.createElement("del");
-// 	tachar.appendChild(subrayado);
-// }
